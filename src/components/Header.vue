50de376b86754b6b3b592befd97d257f7e449c15
Vicente Gutierrez <gtz.rvera@gmail.com>
2021-02-09 12:16:32 -0600

    🎇 Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      title: 'Blume ToDo List'
+    }
+  }
+}
+</script>
