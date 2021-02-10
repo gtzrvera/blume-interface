57aa8f479f873fe59ba8abb166a29dfd04b900db
Vicente Gutierrez <gtz.rvera@gmail.com>
2021-02-10 17:31:26 -0600

    ✅ Add checkbox to mark a task as completed

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="card">
     <div class="card-body">
-      {{ task.name }}
+      <div class="d-inline-block custom-control custom-checkbox">
+        <input
+          v-model="isCompleted"
+          v-bind:name="inputName"
+          v-bind:id="inputName"
+          type="checkbox"
+          class="custom-control-input">
+        <label class="custom-control-label" v-bind:for="inputName"></label>
+      </div>
+      <h5
+        v-bind:class="{ completed: isCompleted }"
+        class="d-inline-block card-title">{{ task.name }}</h5>
     </div>
   </div>
 </template>
@@ -9,11 +20,31 @@
 <script>
 export default {
   name: 'Task',
+  data () {
+    return {
+      isCompleted: false
+    }
+  },
   props: {
     task: {
       type: Object,
       required: true
     }
+  },
+  computed: {
+    inputName () {
+      return `completed_${this.task.id}`;
+    }
   }
 }
 </script>
+
+<style scoped>
+  h5.completed {
+    text-decoration-line: line-through;
+  }
+
+  .card {
+    margin: 5px 0px;
+  }
+</style>
